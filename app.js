c22e6e0665ee1ea18ee139afefa8635a19672394
surelis segarra <sdarianne34@gmail.com>
2020-01-16 11:02:47 -0800

    fixed reset button and increments

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const correctSPan = document.getElementById('correct');
 const totalSpan = document.getElementById('total');
 const resultSpan = document.getElementById('result');
 const sumSpan = document.getElementById('summary');
+const drawSpan = document.getElementById('draw');
 
-let correctGuess = 0;
-let totalOfGuesses = 0;
+let totalWin = 0;
+let totalLose = 0;
+let totalDraw = 0;
+
 
 playButton.addEventListener('click', () => {
     
     const radioButton = document.querySelector('input:checked');
-    const userGuess = radioButton.value;
-    console.log(userGuess);
+    const userInput = radioButton.value;
+    console.log(userInput);
 
-    totalOfGuesses++;
+   
+
     const oneZeroOrTwo = Math.round(Math.random() * 2);
 
     let randomHand;
@@ -28,35 +32,62 @@ playButton.addEventListener('click', () => {
         randomHand = 'scissor'; } 
 
 
-    let won;
-
-    if (randomHand === userGuess){
-        won = true;
-        console.log('i won');
+    if (userInput === 'rock' && oneZeroOrTwo === 0){
+        console.log('draw');
+        totalDraw++;
     }
-    else {
-        won = false; 
+    else if (userInput === 'rock' && oneZeroOrTwo === 1){
+        console.log('lose!');
+        totalLose++;
+    }
+    else if (userInput === 'rock' && oneZeroOrTwo === 2){
+        console.log(' win!');
+        totalWin++;
+    }
+    else if (userInput === 'paper' && oneZeroOrTwo === 1){
+        console.log('draw');
+        totalDraw++;
+    }
+    else if (userInput === 'paper' && oneZeroOrTwo === 0){
+        console.log('win');
+        totalWin++;
+    }
+    else if (userInput === 'paper' && oneZeroOrTwo === 2){
+        console.log('lose');
+        totalLose++;
+    }
+    else if (userInput === 'scissor' && oneZeroOrTwo === 2){
+        console.log('draw');
+        totalDraw++;
+    } 
+    else if (userInput === 'scissor' && oneZeroOrTwo === 1){
+        console.log('win');
+        totalWin++;
+    }   
+    else if (userInput === 'scissor' && oneZeroOrTwo === 0){
+        console.log('lose');
+        totalLose++;
+
     }
 
-    if (won) {
 
-        correctGuess++;
-
-    }
     resultSpan.textContent = randomHand;
     sumSpan.classList.remove('hidden');
-    correctSPan.textContent = correctGuess;
-    totalSpan.textContent = totalOfGuesses;
+    correctSPan.textContent = totalWin;
+    totalSpan.textContent = totalLose;
+    drawSpan.textContent = totalDraw;
 
 });
 
 resetButton.addEventListener('click', () => {
    
-    correctGuess = 0;
-    totalOfGuesses = 0;
+    totalWin = 0;
+    totalLose = 0;
+    totalDraw = 0;
     sumSpan.classList.add('hidden');
-    correctSPan.textContent = correctGuess;
-    totalSpan.textContent = totalOfGuesses;
+    correctSPan.textContent = totalWin;
+    totalSpan.textContent = totalLose;
+    drawSpan.textContent = totalDraw;
 
 
 });
