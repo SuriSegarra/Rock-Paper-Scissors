34be2107a8e686cb279eb25e51eb0f6c0a41f8df
surelis segarra <sdarianne34@gmail.com>
2020-01-15 17:24:30 -0800

    added all html info

---
+++ app.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('play-button');
+button.addEventListener('click', () =>{
+    const checkedRadio = document.getElementById('input:checked');
+    console.log(checkedRadio);
+})
